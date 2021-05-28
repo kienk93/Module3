ec8df6a55769682f19c829376bd4cf101790e398
kienk93 <kienk93@gmail.com>
2021-05-28 15:48:18 +0700

    servlet-calculator

---
+++ servlet-calculator/src/main/java/Servlet/Servletcaculator.java
@@ -0,0 +1,34 @@
+package Servlet;
+
+import Model.Caculator;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servletcaculator", value = "/Servletcaculator")
+public class Servletcaculator extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float firstOperand = Integer.parseInt(request.getParameter("first-operand"));
+        float secondOperand = Integer.parseInt(request.getParameter("second-operand"));
+        char operator = request.getParameter("operator").charAt(0);
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Result:</h1>");
+        try{
+            float result = Caculator.calculate(firstOperand, secondOperand, operator);
+            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
+        }catch (Exception ex){
+            writer.println("Error: " + ex.getMessage());
+        }
+        writer.println("</html>");
+    }
+}
